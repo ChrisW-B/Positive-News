dc35dd3a0d102bb9780b252f6b9e6c5e4e37a9e1
Chris Barry <me@chrisb.xyz>
2017-08-16 15:08:29 -0400

    remove scribe

--- app/index.js
+++ app/index.js
@@ -1,68 +1,74 @@
-var config = require('./config'),
-	sentiment = require('sentiment'),
-	bodyParser = require('body-parser'),
-	request = require('request'),
-	compression = require('compression'),
-	scribe = require('scribe-js')(),
-	express = require('express'),
-	NewsApi = require('news-api-njs'),
-	app = express(),
-	news = new NewsApi({
-		apiKey: config.apiKey
-	});
+require('colors');
+const config = require('./config'),
+  sentiment = require('sentiment'),
+  bodyParser = require('body-parser'),
+  compression = require('compression'),
+  express = require('express'),
+  NewsApi = require('news-api-njs'),
+
+  path = require('path'),
+  winston = require('winston'),
+  expressWinston = require('express-winston'),
+
+  app = express(),
+  news = new NewsApi({
+    apiKey: config.apiKey
+  });
 
 const ONE_MIN = 60 * 1000,
-	ONE_DAY = ONE_MIN * 60 * 24;
-
-
+  ONE_DAY = ONE_MIN * 60 * 24;
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + "/public", {
-	maxAge: ONE_DAY
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(path.join(__dirname, '../public'), {
+  maxAge: ONE_DAY
 }));
 app.use(bodyParser.urlencoded({
-	extended: false
+  extended: false
 }));
 app.use(bodyParser.json());
 app.use(compression());
-app.use(scribe.express.logger(console)); //Log each request
-app.use('/logs', scribe.webPanel());
+app.use(expressWinston.logger({
+  transports: [new winston.transports.Console({ 'timestamp': true, colorize: true })],
+  expressFormat: true,
+  meta: false,
+  colorize: true
+}));
 app.get('/',
-	function(req, res) {
-		news.getSources({
-			language: 'en',
-			country: 'us',
-		}).then(function(response) {
-			res.render('pages/index', {
-				sources: response.sources
-			});
-		}).catch(function(err) {
-			console.log(err);
-		});
-	});
+  function (req, res) {
+    news.getSources({
+      language: 'en',
+      country: 'us'
+    }).then(function (response) {
+      res.render('pages/index', {
+        sources: response.sources
+      });
+    }).catch(function (err) {
+      winston.error(err);
+    });
+  });
 app.get('/news',
-	function(req, res) {
-		console.log(req.query.source);
-		news.getArticles({
-			source: req.query.source
-		}).then(function(response) {
-			response.articles.forEach(function(ele) {
-				var text = [ele.title, ele.description].filter(function(val) {
-					return val;
-				}).join(' '); //avoid giving a null
-				ele.score = sentiment(text).score / text.length;
-			});
-			response.articles.sort(function(a, b) {
-				return b.score - a.score;
-			});
-			response.success = true;
-			res.send(response);
-		}).catch(function(err) {
-			console.log(err);
-		});
-	}
+  function (req, res) {
+    winston.info(req.query.source);
+    news.getArticles({
+      source: req.query.source
+    }).then(function (response) {
+      response.articles.forEach(function (ele) {
+        var text = [ele.title, ele.description].filter(function (val) {
+          return val;
+        }).join(' '); //avoid giving a null
+        ele.score = sentiment(text).score / text.length;
+      });
+      response.articles.sort(function (a, b) {
+        return b.score - a.score;
+      });
+      response.success = true;
+      res.send(response);
+    }).catch(function (err) {
+      winston.error(err);
+    });
+  }
 );
-app.listen(3000, function() {
-	console.log('Positive-News listening on port 3000!');
+app.listen(3000, function () {
+  winston.info('Positive-News listening on port 3000!\n'.rainbow + 'http://localhost:3000/');
 });
